d5841f77f5024e990dacdb58b47f340db361579f
CyberMaryVer <maria.s.startseva@gmail.com>
2021-02-23 15:13:13 +0200

    try heroku

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import numpy as np
-
+import streamlit as st
 
 class Agent:
 
     def __init__(self, n_machines=100):
         self.t = 0  # current time step
         self.n_machines = n_machines
-        self.reward = np.zeros(n_machines)
+        self.reward = np.zeros((n_machines, 2))
 
     def pick_machine(self):
         # returns index of machine
-        if self.t > 5:
-            ind = self.reward.argmax()
-        elif self.t % 5 ==0:
+        np.seterr(divide='ignore', invalid='ignore')
+        if self.t > self.n_machines // 10: # try 10% of all machines
+            ind = (self.reward[:,0]/self.reward[:,1]).argmax()
+        elif self.t % 5 == 0:
             ind = np.random.randint(0, self.n_machines)
         else:
             ind = np.random.randint(0, self.n_machines)
@@ -21,21 +22,21 @@ class Agent:
 
     def get_reward(self, reward, machine_index):
         self.t += 1
-        self.reward[machine_index] += reward
-
+        self.reward[machine_index, 0] += reward
+        self.reward[machine_index, 1] += 1
 
 class Environment:
 
     def __init__(self, n_machines=100):
         self.n_machines = n_machines
-        self.means = means = np.random.normal(1, 1, size=n_machines)
+        self.means = np.random.normal(1, 1, size=n_machines)
 
     def _interact(self, machine_index):
         assert 0 <= machine_index < self.n_machines, 'Bad machine index'
         reward = np.random.normal(self.means[machine_index], 1)
         return reward
 
-    def run(self, time_steps=1000, n=5, verbose=False):
+    def run(self, time_steps=1000, n=100, verbose=False):
         total_reward = 0
         agent = Agent(n)
         self.n_machines = agent.n_machines #########################
@@ -48,8 +49,9 @@ class Environment:
 
         if verbose:
             print(agent.reward)
+            # print(self.means)
         return total_reward
 
 
 np.random.seed(0)
-print(Environment().run(n=100, verbose=False))
+st.write(Environment().run(verbose=False))
