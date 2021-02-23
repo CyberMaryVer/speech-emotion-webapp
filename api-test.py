3d26997d98bf3a43c0923609d9bd6c332465ff5f
CyberMaryVer <maria.s.startseva@gmail.com>
2021-02-23 16:00:11 +0200

    try heroku

--- api-test.py
+++ api-test.py
@@ -19,34 +19,34 @@ CAT6 = ['fear', 'angry', 'neutral', 'happy', 'sad', 'surprise']
 CAT3 = ["positive", "neutral", "negative"]
 
 # page settings
-# st.set_page_config(layout="wide")
-#
-# max_width = 1000
-# padding_top = 0
-# padding_right = "20%"
-# padding_left = "10%"
-# padding_bottom = 0
-# COLOR = "#1f1f2e"
-# BACKGROUND_COLOR = "#d1d1e0"
-#
-# st.markdown(
-#         f"""
-# <style>
-#     .reportview-container .main .block-container{{
-#         max-width: {max_width}px;
-#         padding-top: {padding_top}rem;
-#         padding-right: {padding_right}rem;
-#         padding-left: {padding_left}rem;
-#         padding-bottom: {padding_bottom}rem;
-#     }}
-#     .reportview-container .main {{
-#         color: {COLOR};
-#         background-color: {BACKGROUND_COLOR};
-#     }}
-# </style>
-# """,
-#         unsafe_allow_html=True,
-#     )
+st.set_page_config(layout="wide")
+
+max_width = 1000
+padding_top = 0
+padding_right = "20%"
+padding_left = "10%"
+padding_bottom = 0
+COLOR = "#1f1f2e"
+BACKGROUND_COLOR = "#d1d1e0"
+
+st.markdown(
+        f"""
+<style>
+    .reportview-container .main .block-container{{
+        max-width: {max_width}px;
+        padding-top: {padding_top}rem;
+        padding-right: {padding_right}rem;
+        padding-left: {padding_left}rem;
+        padding-bottom: {padding_bottom}rem;
+    }}
+    .reportview-container .main {{
+        color: {COLOR};
+        background-color: {BACKGROUND_COLOR};
+    }}
+</style>
+""",
+        unsafe_allow_html=True,
+    )
 
 
 @st.cache
@@ -258,4 +258,4 @@ if __name__ == '__main__':
 # rerun.
 
 
-# st.button("Re-run")
+st.button("Re-run")
